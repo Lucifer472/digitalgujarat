6c51c20b7eb624b53b031b65dc274accab8ee414
Lucifer472 <hardiksadhu472@gmail.com>
2024-12-12 12:21:46 +0530

    Version 1

---
+++ components/ads/ads.tsx
@@ -0,0 +1,22 @@
+"use client";
+import { AdsWrapper } from "@/components/wrappers/ad-wrapper";
+
+export const Ad1 = () => {
+  return <AdsWrapper id="" />;
+};
+
+export const Ad2 = () => {
+  return <AdsWrapper id="" />;
+};
+
+export const Ad3 = () => {
+  return <AdsWrapper id="" />;
+};
+
+export const Ad4 = () => {
+  return <AdsWrapper id="" />;
+};
+
+export const Ad5 = () => {
+  return <AdsWrapper id="" />;
+};
