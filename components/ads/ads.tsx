ef987bcdacb58625c6cd27c09b945bed6bef35c5
Lucifer472 <hardiksadhu472@gmail.com>
2025-02-24 12:00:27 +0530

    added suspense boundary

--- components/ads/ads.tsx
+++ components/ads/ads.tsx
@@ -1,47 +1,58 @@
 "use client";
 import { AdsWrapper } from "@/components/wrappers/ad-wrapper";
+import { Suspense } from "react";
 
 export const Ad1 = () => {
   return (
-    <AdsWrapper
-      id="MS_Digitalgujarat_300X250"
-      slot="/22989534981/MS_Digitalgujarat_300X250"
-    />
+    <Suspense>
+      <AdsWrapper
+        id="MS_Digitalgujarat_300X250"
+        slot="/22989534981/MS_Digitalgujarat_300X250"
+      />
+    </Suspense>
   );
 };
 
 export const Ad2 = () => {
   return (
-    <AdsWrapper
-      id="MS_Digitalgujarat_300X250_1"
-      slot="/22989534981/MS_Digitalgujarat_300X250_1"
-    />
+    <Suspense>
+      <AdsWrapper
+        id="MS_Digitalgujarat_300X250_1"
+        slot="/22989534981/MS_Digitalgujarat_300X250_1"
+      />
+    </Suspense>
   );
 };
 
 export const Ad3 = () => {
   return (
-    <AdsWrapper
-      id="MS_Digitalgujarat_300X250_2"
-      slot="/22989534981/MS_Digitalgujarat_300X250_2"
-    />
+    <Suspense>
+      <AdsWrapper
+        id="MS_Digitalgujarat_300X250_2"
+        slot="/22989534981/MS_Digitalgujarat_300X250_2"
+      />
+    </Suspense>
   );
 };
 
 export const Ad4 = () => {
   return (
-    <AdsWrapper
-      id="MS_Digitalgujarat_300X250_3"
-      slot="/22989534981/MS_Digitalgujarat_300X250_3"
-    />
+    <Suspense>
+      <AdsWrapper
+        id="MS_Digitalgujarat_300X250_3"
+        slot="/22989534981/MS_Digitalgujarat_300X250_3"
+      />
+    </Suspense>
   );
 };
 
 export const Ad5 = () => {
   return (
-    <AdsWrapper
-      id="MS_Digitalgujarat_300X250_4"
-      slot="/22989534981/MS_Digitalgujarat_300X250_4"
-    />
+    <Suspense>
+      <AdsWrapper
+        id="MS_Digitalgujarat_300X250_4"
+        slot="/22989534981/MS_Digitalgujarat_300X250_4"
+      />
+    </Suspense>
   );
 };
